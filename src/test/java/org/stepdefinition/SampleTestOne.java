f95748dc6c5acbf98159de91dfd67184df23d61d
DhanushaSharp <dhanushadhanusha2110@gmail.com>
2024-01-08 18:30:42 -0800

    SampleTestOne class added

---
+++ src/test/java/org/stepdefinition/SampleTestOne.java
@@ -0,0 +1,43 @@
+package org.stepdefinition;
+
+import org.testng.annotations.Test;
+
+public class SampleTestOne {
+	
+	@Test
+	private void tc7() {
+		System.out.println("Test case 7");
+
+	}
+	@Test
+	private void tc2() {
+		System.out.println("Test case 2");
+
+	}
+	@Test
+	private void tc4() {
+		System.out.println("Test case 4");
+
+	}
+	@Test
+	private void tc6() {
+		System.out.println("Test case 6");
+
+	}
+	@Test
+	private void tc3() {
+		System.out.println("Test case 3");
+
+	}
+	@Test
+	private void tc1() {
+		System.out.println("Test case 1");
+
+	}
+	@Test
+	private void tc5() {
+		System.out.println("Test case 5");
+
+	}
+
+}
